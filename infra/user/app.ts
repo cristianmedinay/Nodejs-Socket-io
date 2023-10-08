d81c5d3e356d81ef73699e90ec9d54cc0fa92698
cristian <cristianmedinay@gmail.com>
2023-10-08 23:31:33 +0200

    users

--- infra/user/app.ts
+++ infra/user/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { createServer } from 'http'  
+import { Server } from 'socket.io'
 import cors from 'cors'
 import morgan from 'morgan'
 import authRoutes from './infrastructure/router/auth.routes'
@@ -17,9 +19,16 @@ const app = express();
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: ['http://localhost:5173',"http://127.0.0.1:3000"]
+    }
+});
+
 app.use(cookies());
 app.use(cors({
-    origin: ['http://localhost:3000','http://192.168.1.37:3000','http://127.0.0.1:3000','http://localhost:3001'],
+    origin: ['http://localhost:3000','http://192.168.1.37:3000','http://127.0.0.1:3000','http://localhost:3001','http://localhost:5173'],
     credentials: true
 }));
 app.use(morgan('dev'))
@@ -29,9 +38,37 @@ console.log(process.env.KEYS)
 app.use(passport.initialize())
 passport.use(passportMiddleware)
 //routes
+
+interface User {
+    name: string;
+}
 /* app.get('/',(req,res)=>{
     res.send(`The api is at http://localhost:${app.get('port')}`)
 }) */
 app.use(authRoutes);
 app.use(specialRoutes);
-export default app;
+let users: User[] = [{'name':'this.nombreUsuario1696780753597' },{ 'name':'this.nombreUsuario1696782325330'}]
+let datos:any = [];
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('increment', (arg) => {
+        datos.push(arg)
+        /* const arrayActualizado = users.map((e)=>{
+          return e.name==arg.nombreRemitente ? arg : e     
+        }) */
+        
+        /* console.log(arg); */
+        if(datos.length>1){
+        const indiceAReemplazar = datos.findIndex((objeto:any) => objeto.remitente === arg.remitente);
+        console.log(indiceAReemplazar)
+        if (indiceAReemplazar !== -1) {            
+            datos[indiceAReemplazar] = arg;        
+        }
+        const mensajesSinDuplicados = Array.from(new Set(datos.map((mensaje:any) => mensaje.remitente))).map((remitente:any) => datos.find((mensaje:any) => mensaje.remitente === remitente));
+        console.log(mensajesSinDuplicados);
+        }
+
+        io.emit('nuevoMensaje', JSON.stringify(arg));
+    });
+  });
+export { app,io};
--- infra/user/index.ts
+++ infra/user/index.ts
@@ -1,5 +1,6 @@
-import app from './app'
+import {app,io} from './app'
 import dbInit from './infrastructure/database/database'
 const port =  process.env.PORT || 3000
 dbInit()
+io.listen(4000);
 app.listen(port,()=>console.log(`Listo por el puerto ${port}`));
